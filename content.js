298f087300c5d388425ea9dc047a290439f2de44
Jim Qiu <bluefisher80@gmail.com>
2018-11-17 22:00:41 +0800

    init

---
+++ content.js
@@ -0,0 +1,8 @@
+var node = document.createElement("a");
+var br = document.createElement("br");
+node.href="https://www.google.com/";
+node.text="点击这里试试用Google吧";
+document.getElementById("qrcode").appendChild(node);
+document.getElementById("qrcode").appendChild(br);
+
+
